a1c2a1eb8d09d59eecfb12d38072600d5de093e1
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-20 14:52:14 +0000

    update code

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 0's module.
+'''Module for Task 0
 '''
 import asyncio
 import random
@@ -7,8 +7,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-        '''Generates a sequence of 10 numbers.
-            '''
-                for _ in range(10):
-                            await asyncio.sleep(1)
-                                    yield random.random() * 10
+    '''Generates a sequence of 10 numbers.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
